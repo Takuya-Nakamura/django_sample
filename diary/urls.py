c0591b1089a74f75a268cf1de8b7e633d18b927c
TakuyaNakamura <nakamura0803@gmail.com>
2020-07-14 17:52:40 +0900

    mod routing. view.py. response json

--- diary/urls.py
+++ diary/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from .views import ApiView
 
 app_name = 'diary'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('api', ApiView.as_view(), name='index2'),
+  path('api_test', views.api_test, name='index2'),
+
 ] 
